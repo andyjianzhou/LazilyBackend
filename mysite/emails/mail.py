28cb9a31130c404a36910c04940e464b8dae74b0
PandaOnTheFly <74941296+andyjianzhou@users.noreply.github.com>
2022-11-22 16:42:34 -0700

    fixed emails functionality
    Need to connect SMPT and add custom email

--- mysite/emails/mail.py
+++ mysite/emails/mail.py
@@ -2,10 +2,8 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def send_email(email):
-    print("starting to send email")
     subject = 'Welcome to my site'
     message = f'Hi {email}, thanks for signing up'
-    print(message)
     email_from = settings.EMAIL_HOST_USER
     print(email_from)
     recipient_list = [email,]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import corsheaders
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ld*-mx#732m179v_e8v-n$*6eiv)z!2syj59d+ytl(*h)$@%ko'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -141,8 +142,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
-EMAIL_PORT = '8080'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = '' # add less secure app here later
+EMAIL_HOST_PASSWORD = '' # add less secure app pass here later
+EMAIL_USE_TLS = True
